8ff2e3bb359867c62eb3e74d936fd17e4849484c
cloudgyb <gyb_cloud@163.com>
2025-02-25 17:35:27 +0800

    实现启动类

--- src/main/java/com/github/cloudgyb/jerry/Jerry.java
+++ src/main/java/com/github/cloudgyb/jerry/Jerry.java
@@ -1,10 +1,49 @@
 package com.github.cloudgyb.jerry;
 
+import com.github.cloudgyb.jerry.http.JerryHttpServer;
+
+import java.io.IOException;
+//@formatter:off
 /**
- * Hello world!
+ * Jerry的主类，负责启动HTTP服务器
+ *                            _ooOoo_
+ *                           o8888888o
+ *                           88" . "88
+ *                           (| -_- |)
+ *                           O\  =  /O
+ *                        ____/`---'\____
+ *                      .'  \\|     |    ‘.
+ *                     /  \|||  :  |||  \
+ *                     )__(  .___.'  )__(
+ *                     `---`|     |`---`
+ *                       |   -   -   |
+ *                       \  .-.-  /
+ *                    ___`. .'  `. .__
+ *                  ."" '<  `.___\_<|>_/___.'  >'"".
+ *                 | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+ *                 \  \ `-.   \_ __\ /__ _/   .-` /  /
+ *           ======`-.____`-.___\_____/___.-`____.-'======
+ *                              `=---='
+ *           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ *                     佛祖保佑        永无BUG
+ *            佛曰:
+ *                   写字楼里写字间，写字间里程序员；
+ *                   程序人员写程序，又拿程序换酒钱。
+ *                   酒醒只在网上坐，酒醉还来网下眠。
+ *                   酒醉酒醒日复日，网上网下年复年。
+ *                   但愿老死电脑间，不愿鞠躬老板前；
+ *                   奔驰宝马贵者趣，公交自行程序员。
+ *                   别人笑我太疯癫，我笑自己命更贱；
+ *                   不见满街漂亮妹，哪来一身好技术。
  */
+//@formatter:on
 public class Jerry {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        JerryHttpServer jerryHttpServer = new JerryHttpServer(8888);
+        jerryHttpServer.start();
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("stopping");
+            jerryHttpServer.stop();
+        }));
     }
 }
--- src/main/java/com/github/cloudgyb/jerry/http/JerryHttpServer.java
+++ src/main/java/com/github/cloudgyb/jerry/http/JerryHttpServer.java
@@ -5,10 +5,13 @@ import com.github.cloudgyb.jerry.servlet.HelloServlet;
 import com.github.cloudgyb.jerry.servlet.ServletContextImpl;
 import com.github.cloudgyb.jerry.servlet.filter.TestFilter;
 import com.sun.net.httpserver.HttpServer;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletRegistration;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Executors;
 
 /**
@@ -16,27 +19,52 @@ import java.util.concurrent.Executors;
  * @since 2025/2/10 20:45
  */
 public class JerryHttpServer {
-    private static final String contextPath = "/";
+    private static final String defaultContextPath = "/";
+    private final InetSocketAddress address;
+    private final HttpServer httpServer;
+    private final Map<String, ServletContextImpl> servletContextMap = new HashMap<>();
 
-    public static void main(String[] args) throws IOException {
-        ServletContextImpl servletContext = new ServletContextImpl(contextPath);
+    public JerryHttpServer(InetSocketAddress address) throws IOException {
+        this.address = address;
+        this.httpServer = HttpServer.create(address, 512);
+    }
+
+    public JerryHttpServer(int port) throws IOException {
+        this(new InetSocketAddress(port));
+    }
+
+    public JerryHttpServer(String host, int port) throws IOException {
+        this(new InetSocketAddress(host, port));
+    }
+
+    public void start() {
+        ServletContextImpl servletContext = createDefaultServletContext();
+        servletContextMap.put(servletContext.getContextPath(), servletContext);
+        JerryHttpHandler jerryHttpHandler = new JerryHttpHandler(servletContext);
+        httpServer.setExecutor(Executors.newSingleThreadExecutor());
+        httpServer.createContext(servletContext.getContextPath(), jerryHttpHandler);
+        httpServer.start();
+        System.out.printf("JerryServer is running! It listen at %s:%d%n",
+                address.getHostString(), address.getPort());
+    }
+
+    public void stop() {
+        httpServer.stop(2);
+        servletContextMap.clear();
+        System.out.println("JerryServer is stopped!");
+    }
+
+    private static ServletContextImpl createDefaultServletContext() {
+        ServletContextImpl servletContext = new ServletContextImpl(defaultContextPath);
         ServletRegistration.Dynamic dynamic = servletContext.addServlet("default", DefaultServlet.class);
         dynamic.addMapping("/");
 
         servletContext.addServlet("/hello", HelloServlet.class);
         servletContext.addFilter("/*", TestFilter.class);
+        return servletContext;
+    }
 
-        JerryHttpHandler jerryHttpHandler = new JerryHttpHandler(servletContext);
-        InetSocketAddress inetSocketAddress = new InetSocketAddress(8888);
-        HttpServer httpServer = HttpServer.create(inetSocketAddress, 512);
-        httpServer.setExecutor(Executors.newSingleThreadExecutor());
-        httpServer.createContext(contextPath, jerryHttpHandler);
-        httpServer.start();
-        System.out.printf("JerryServer is running! It listen at %s:%d%n",
-                inetSocketAddress.getHostString(), inetSocketAddress.getPort());
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            System.out.println("stopping");
-            httpServer.stop(5);
-        }));
+    public ServletContext getServletContext(String contextPath) {
+        return servletContextMap.get(contextPath);
     }
 }
